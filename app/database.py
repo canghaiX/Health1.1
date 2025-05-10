4a4e661571d29af10ef1cceb2bc05759c9cb9f23
PengKangKangQAQ <1415556032@qq.com>
2025-05-10 21:44:27 +0800

    修改数据库配置

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:root@127.0.0.1:3306/tesst"
+DATABASE_URL = "mysql+pymysql://root:root@127.0.0.1:3306/health"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
