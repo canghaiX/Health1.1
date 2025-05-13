64adf8403e59f8e463859a4cc4900787fb736854
PengKangKangQAQ <1415556032@qq.com>
2025-05-14 02:46:07 +0800

    新增雷达波sql

--- health.sql
+++ health.sql
@@ -49,3 +49,36 @@ CREATE TABLE `hra_data`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+
+
+DROP TABLE IF EXISTS `radar_wave`;
+CREATE TABLE `radar_wave`  (
+  `id` int(11) NOT NULL COMMENT 'id，标识',
+  `csgCollectId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '样例：a8afa745-819c-46a0-aea3-02cfd4de3884',
+  `user_id` int(11) NOT NULL COMMENT '关联到用户',
+  `equipment_id` int(11) NULL DEFAULT NULL COMMENT '设备id',
+  `startTime` datetime(0) NOT NULL COMMENT '开始时间戳，毫秒级',
+  `endTime` datetime(0) NOT NULL COMMENT '结束时间戳，毫秒级',
+  `heartRate` int(11) NOT NULL COMMENT '心率',
+  `heartConclusion` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '心脏诊断结果',
+  `breathConclusion` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '呼吸诊断结果',
+  `sti` float NOT NULL COMMENT '应该是某种指标',
+  `sdnn` float NOT NULL COMMENT '心脏相关',
+  `rmssd` float NOT NULL COMMENT '心脏相关',
+  `pnn50` float NOT NULL COMMENT '心脏相关',
+  `cvsd` float NOT NULL COMMENT '心脏相关',
+  `cvcdi` float NOT NULL COMMENT '心脏相关',
+  `lf` float NOT NULL COMMENT '心脏相关',
+  `hf` float NOT NULL COMMENT '心脏相关',
+  `ifHfRatio` float NOT NULL COMMENT '心脏相关',
+  `breathRate` int(11) NOT NULL COMMENT '呼吸频率',
+  `avgBreathDepth` float NOT NULL COMMENT '平均呼吸深度',
+  `maxBreathDepth` float NOT NULL COMMENT '最大呼吸深度',
+  `minBreathDepth` float NOT NULL COMMENT '最小呼吸深度',
+  `expiratoryTime` int(11) NOT NULL COMMENT '呼吸机呼气时间',
+  `inspiratoryTime` int(11) NOT NULL COMMENT '吸气时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
