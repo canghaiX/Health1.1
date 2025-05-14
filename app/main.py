29fb13a5123044d3f9a2d55711f34d8d865e3b75
PengKangKangQAQ <1415556032@qq.com>
2025-05-15 04:08:04 +0800

    5.15 中间件实现跨域，添加对话子路由

--- app/main.py
+++ app/main.py
@@ -1,15 +1,25 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routers.userRouters import userRouter
 from app.routers import hrafile_router
+from app.routers.conversation_router import conversation_router
 from app.utils.Interceptors import RequestInterceptor
 
 app = FastAPI()
 app.include_router(userRouter, prefix="/user", tags=["用户功能模块"])
+app.include_router(conversation_router)
 app.include_router(hrafile_router.router)
 
 app.add_middleware(RequestInterceptor)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
 @app.get("/home")
 async def home():
     return {"message": "hello world~"}
